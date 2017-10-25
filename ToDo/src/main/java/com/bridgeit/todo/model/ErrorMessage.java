b98d879d93cca5cab3c3de597f93c73a9ce655b2
MFiroz303 <asfiroz007@gmail.com>
2017-10-25 19:30:58 +0530

    Backend

---
+++ ToDo/src/main/java/com/bridgeit/todo/model/ErrorMessage.java
@@ -0,0 +1,15 @@
+package com.bridgeit.todo.model;
+
+public class ErrorMessage {
+	
+	private String responseMessage;
+
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+
+}
